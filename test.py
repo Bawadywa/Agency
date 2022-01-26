928426b2286395d51e4dee0e5bfde467e04a03aa
Bawadywa <o.oxi@ukr.net>
2022-01-26 23:05:46 +0200

    bug fixing

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 # +
 import datetime
-import time
+import pandas as pd
+import re
+import PyPDF2
 
 from RPA.Browser.Selenium import Selenium
-from RPA.Excel.Files import Files
-from RPA.JSON import JSON
 from RPA.FileSystem import FileSystem
+from RPA.JSON import JSON
 
 browser = Selenium()
 
@@ -16,20 +17,13 @@ excel_name = "Report.xlsx"
 agency_name = JSON().load_json_from_file("devdata/env.json")["agency_name"]
 
 
-def wait(seconds: int):
-    time.sleep(seconds)
-
-
 def open_url(url: str):
     browser.open_available_browser(url)
     browser.maximize_browser_window()
 
 
 def dive_in():
-    dive_in_btn = browser.find_element(locator="xpath:/html/body/main/div[1]/div/div/div[3]/"
-                                               "div/div/div/div/div/div/div/div/div/a")
-    # find dive in button
-    browser.click_button(dive_in_btn)
+    browser.click_link("#home-dive-in")
     browser.wait_until_page_contains_element(locator="css:div#agency-tiles-widget span")
     # wait until agencies table appears on the page
 
@@ -55,106 +49,33 @@ def get_agencies() -> dict:
 
 
 def fill_excel_agencies(workbook_path: str, data: dict):
-    lib = Files()
-    workbook = lib.create_workbook(path=workbook_path)
-
-    row, col = 1, 1
-    for key, value in data.items():
-        # filling worksheet with agency names and amounts
-        workbook.set_cell_value(row, col, key)
-        workbook.set_cell_value(row, col + 1, value)
-        row += 1
-
-    workbook.rename_worksheet("Agencies", workbook.sheetnames[0])
-    # rename first worksheet with "Agencies"
-
-    lib.save_workbook(path=workbook_path)
+    header = ["Agency", "Amount"]
+    data_frame = pd.DataFrame(list(data.items()))
+    data_frame.columns = header
+    data_frame.to_excel(workbook_path, "Agencies", index=False)
 
 
 def open_agency(name: str):
-    btn = browser.find_element(locator="xpath://span[contains(text(), \"{}\")]/parent::*".format(name))
-    # find parent link of the span with agency name to follow the link
-    browser.click_link(btn)
+    browser.click_link(locator="partial link:{}".format(name))
     browser.wait_until_page_contains_element(locator="id:investments-table-object_wrapper",
-                                             timeout=datetime.timedelta(seconds=10))
+                                             timeout=datetime.timedelta(seconds=15))
     # wait until investments table appears
 
 
 def show_all_entries():
+    showing_elem = browser.find_element(locator="id:investments-table-object_info")
+    showing_text = browser.get_text(showing_elem)   # get showing text to compare in wait until
     browser.select_from_list_by_label("name:investments-table-object_length", "All")
     # select all entries of investments
-    wait(10)
+    browser.wait_until_element_does_not_contain(locator=showing_elem, text=showing_text,
+                                                timeout=datetime.timedelta(seconds=15))
     # wait until investments table refreshes
 
 
-def get_table_header() -> list:
-    header_table = browser.find_element(locator="xpath://*[@id=\"investments-table-object_wrapper\"]"
-                                                "/div[3]/div[1]/div/table")
-    # find header table to get titles for excel file
-
-    data = []
-    i = 1
-    while True:     # using while true with try except for getting all the rows of the table
-        data.append([])
-        try:
-            for j in range(1, 8):
-                data[i - 1].append(browser.get_table_cell(header_table, i, j))
-        except Exception:
-            data.pop(-1)    # deleting last row because it'll be empty
-            break
-        i += 1
-
-    header = data[-1]   # last row contains titles
-    return header
-
-
-def get_table_data() -> list:
-    table = browser.find_element(locator="id:investments-table-object")
-    # find table using id
-
-    data = []
-    i = 1
-    while True:     # using while true with try except for getting all the rows of the table
-        data.append([])
-        try:
-            for j in range(1, 8):
-                data[i-1].append(browser.get_table_cell(table, i, j))
-        except Exception:
-            data.pop(-1)    # deleting last row because it'll be empty
-            break
-        i += 1
-
-    result_data = []
-    # this for cycle means making result list without rows which are empty (having empty strings - "")
-    for row in data:
-        if row.count("") == len(row):
-            continue
-        else:
-            result_data.append(row)
-
-    return result_data
-
-
-def fill_excel_investments(workbook_path: str, worksheet_name: str, header: list, data: list):
-    lib = Files()
-    workbook = lib.open_workbook(workbook_path)
-    # opens already created workbook
-    workbook.create_worksheet(worksheet_name)
-    # creates new worksheet for investments
-
-    row, col = 1, 1
-    for title in header:
-        # filling first row of excel as header
-        workbook.set_cell_value(row, col, title)
-        col += 1
-
-    # filling main data
-    for row in range(len(data)):
-        for col in range(len(data[row])):
-            workbook.set_cell_value(row + 2, col + 1, data[row][col])   
-            # using row + 2 because first row is header
-
-    lib.save_workbook(path=workbook_path)
+def html_table_to_excel(workbook_path: str, worksheet_name: str):
+    data = pd.read_html(browser.get_source(), match="UII")
+    with pd.ExcelWriter(workbook_path, engine="openpyxl", mode="a") as writer:
+        data[1].to_excel(writer, worksheet_name, index=False)
 
 
 def get_investments_links() -> list:
@@ -173,32 +94,79 @@ def download_pdf(links: list):
         pdf_link_parent = browser.find_element(locator="business-case-pdf")
         pdf_link = browser.find_element(locator="tag:a", parent=pdf_link_parent)
         browser.click_link(locator=pdf_link)
-        wait(10)
+        browser.wait_until_page_does_not_contain_element(locator="css:div#business-case-pdf span",
+                                                         timeout=datetime.timedelta(seconds=10))
         # wait for pdf to generate
         browser.switch_window("MAIN")
         # switch to main tab
-        wait(5)
+        try:
+            browser.wait_until_page_does_not_contain_element(locator="id:main-content",
+                                                             timeout=datetime.timedelta(seconds=2))
+        except:
+            continue
+        # wait until file is downloaded
+
+
+def compare_pdfs_excel(path: str, workbook_path: str, worksheet_name: str):
+    pdf_files = FileSystem().find_files(path + r"*.pdf")
+    pdf_paths = list(map(lambda file: file.path, pdf_files))
+    name_pattern, uii_pattern = r"(?<=1\. Name of this Investment:\n \n)[^\n]+",\
+                                r"(?<=2\. Unique Investment Identifier \(UII\):\n \n)[^\n]+"
+    pdf_data = []
+    for path in pdf_paths:
+        pdf_reader = PyPDF2.PdfFileReader(path)
+        text = pdf_reader.getPage(0).extractText()
+        match_name, match_uii = re.search(name_pattern, text), re.search(uii_pattern, text)
+        name, uii = "", ""
+        if match_name is not None:
+            name = match_name.group(0)
+
+        if match_uii is not None:
+            uii = match_uii.group(0)
+        pdf_data.append({"name": name, "uii": uii})
+
+    data_frame = pd.read_excel(workbook_path, worksheet_name)
+    pd.set_option("display.max_columns", 20)
+    records = list(data_frame.to_dict("records"))
+
+    print("-"*5 + "{}".format("Comparing pdf and excel") + "-"*5)
+    record_index = 0
+    for record in records:
+        for index in range(len(pdf_data)):
+            if record["UII"] == pdf_data[index]["uii"] and record["Investment Title"] == pdf_data[index]["name"]:
+                print("Row Index = {}, Name of Investment: {}, UII: {}\n{}"
+                      .format(record_index + 2, *pdf_data[index].values(), record), end="\n\n")
+                pdf_data.remove(pdf_data[index])
+                break
+                # + 2 to show exact row index from excel
+        record_index += 1
+
+
+def delete_pdfs(path: str):
+    FileSystem().remove_files(*list(map(lambda file: file.path,
+                                        FileSystem().find_files(path + "*.pdf"))))
 
 
 def first_page(path: str):
     open_url(main_url)
     dive_in()
-    fill_excel_agencies(workbook_path=r"{}{}".format(path, excel_name), data=get_agencies())
+    fill_excel_agencies(workbook_path=r"{}{}".format(path, excel_name),
+                        data=get_agencies())
 
 
 def second_page(path: str):
     open_agency(agency_name)
     show_all_entries()
-    fill_excel_investments(workbook_path=r"{}{}".format(path, excel_name),
-                           worksheet_name=agency_name,
-                           header=get_table_header(),
-                           data=get_table_data())
+    html_table_to_excel(r"{}{}".format(path, excel_name), agency_name)
     download_pdf(links=get_investments_links())
+    compare_pdfs_excel(path=path, workbook_path=r"{}{}".format(path, excel_name),
+                       worksheet_name=agency_name)
 
 
 def main(path: str):
     try:
         browser.set_download_directory(path)
+        delete_pdfs(path)
         first_page(path)
         second_page(path)
     finally:
